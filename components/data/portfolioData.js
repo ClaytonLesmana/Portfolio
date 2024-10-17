b0b98e3cbe8d459eab24c3dbcfbac4b7ecbb2455
claytonlesmana <claytonlesmana0808@gmail.com>
2024-10-17 22:08:40 +1100

    Portfolio Project

---
+++ components/data/portfolioData.js
@@ -0,0 +1,26 @@
+const portfolioData = [
+    {
+      id: 1,
+      title: "Fullstack",
+      category: "Fullstack",
+      image: "../assets/profile.jpg",
+      description: "A brand project involving tag designs."
+    },
+    {
+      id: 2,
+      title: "Frontend",
+      category: "Frontend",
+      image: "../assets/LOGO.png",
+      description: "Creative bag mockup for showcasing branding."
+    },
+    {
+      id: 3,
+      title: "Backend",
+      category: "Backend",
+      image: "../assets/profile.jpg",
+      description: "A photography session for product packaging."
+    },
+    // Add more items
+  ];
+
+  export default portfolioData
