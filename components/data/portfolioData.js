f93b576a8fe653bc998968c3250267bb9fc8e605
claytonlesmana <claytonlesmana0808@gmail.com>
2024-10-24 22:21:06 +1100

    tes

--- components/data/portfolioData.js
+++ components/data/portfolioData.js
@@ -6,7 +6,7 @@ const portfolioData = [
       id: 1,
       title: "Fullstack",
       category: "Fullstack",
-      image: profileImage,
+      // image: profileImage,
       description: "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
       technologies: ["HTML", "CSS", "JavaScript","react","bootstrap","node.js"],
       URL:"www.google.com",
@@ -16,7 +16,7 @@ const portfolioData = [
       id: 2,
       title: "Frontend",
       category: "Frontend",
-      image: profileImage,
+      // image: profileImage,
       description: "Creative bag mockup for showcasing branding.",
       technologies: ["HTML", "CSS", "JavaScript"]
     },
@@ -24,7 +24,7 @@ const portfolioData = [
       id: 3,
       title: "Backend",
       category: "Backend",
-      image: profileImage,
+      // image: profileImage,
       description: "A photography session for product packaging.",
       technologies: ["HTML", "CSS", "JavaScript"]
     },
