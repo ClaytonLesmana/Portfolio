3630765b2a49a46693ab0b9bd8c06d3e56dbb54a
claytonlesmana <claytonlesmana0808@gmail.com>
2024-10-25 13:05:41 +1100

    added thank you message

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
 import CallIcon from "@mui/icons-material/Call";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import BadgeIcon from "@mui/icons-material/Badge";
 import { useAnimateUnderline } from "../components/hook/ useAnimateUnderline";
 
-function contact() {
+function Contact() {
   const underlineRef = useAnimateUnderline();
+  const [showThankYou, setShowThankYou] = useState(false);
+
   const onSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -27,6 +29,10 @@ function contact() {
 
     if (res.success) {
       console.log("Success", res);
+      setShowThankYou(true);
+      setTimeout(() => {
+        setShowThankYou(false);
+      }, 4000); // Hide message after 4 seconds
     }
   };
 
@@ -43,17 +49,20 @@ function contact() {
           {/* Left - Message Form */}
           <div className="message-form">
             <h3>Message Me</h3>
-            <form>
-              <div className="input-group">
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email" />
+            <div className="input-group">
+              <input type="text" placeholder="Name" name="name" required />
+              <input type="email" placeholder="Email" name="email" required />
+            </div>
+            <input type="text" placeholder="Subject" name="subject" required />
+            <textarea placeholder="Message" name="message" required></textarea>
+            <button type="submit" className="send-button">
+              Send Message
+            </button>
+            {showThankYou && (
+              <div className="thank-you-message">
+                Thank you for your message! I will get back to you soon.
               </div>
-              <input type="text" placeholder="Subject" />
-              <textarea placeholder="Message"></textarea>
-              <button type="submit" className="send-button">
-                Send Message
-              </button>
-            </form>
+            )}
           </div>
         </form>
 
@@ -99,4 +108,4 @@ function contact() {
   );
 }
 
-export default contact;
+export default Contact;
